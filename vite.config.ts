989cccdfad1b2de6c613e97733fdfcf3408eede7
YuanSa <yuansasi@outlook.com>
2022-05-18 00:52:23 +0800

    feat: save

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve as resolvePath } from "path";
+import { readFileSync } from "fs";
+
+const Port = 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +14,12 @@ export default defineConfig({
   },
   plugins: [react()],
   assetsInclude: ["**/*.md"],
+  server: {
+    port: Port,
+    open: `https://localhost:${Port}`,
+    https: {
+      pfx: readFileSync("./chore/cert.pfx"),
+      passphrase: "Yuansa0130",
+    },
+  },
 });
