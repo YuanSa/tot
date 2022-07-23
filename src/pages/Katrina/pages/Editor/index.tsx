2234f13c100e4659d7f4113c89b28a6193e41a2d
YuanSa <yuansasi@outlook.com>
2022-07-24 00:49:39 +0800

    feat: add dnd

--- src/pages/Katrina/pages/Editor/index.tsx
+++ src/pages/Katrina/pages/Editor/index.tsx
@@ -1,12 +1,12 @@
 import { Button, Collapse, Form, TabPane, Tabs } from "@douyinfe/semi-ui";
 import { IconClose, IconPlus, IconMinus } from "@douyinfe/semi-icons";
 import { ArrayField, FormApi as FormAPI } from "@douyinfe/semi-ui/lib/es/form";
-import { FC, useRef } from "react";
+import { FC, useRef, useState } from "react";
 import { Katrina } from "../../types";
 import { useT } from "@/utils/language";
-import styles from "./styles.module.scss";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import classNames from "classnames";
-import { DragDropContext } from "react-beautiful-dnd";
+import styles from "./styles.module.scss";
 
 interface PageKatrinaEditorProps {
   initFile: Katrina;
@@ -18,6 +18,30 @@ export const PageKatrinaEditor: FC<PageKatrinaEditorProps> = ({
 }) => {
   const t = useT();
   const formAPI = useRef<FormAPI>();
+  const [itemsExpanded, setItemsExpanded] = useState<string[]>([]);
+
+  const handleDragItemEnd: DragDropContext["props"]["onDragEnd"] = ({
+    destination,
+    source,
+    draggableId,
+  }) => {
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const targetValue = formAPI.current?.getValue(draggableId);
+    const newValue = formAPI.current?.getValue(destination.droppableId);
+    newValue.splice(source.index, 1);
+    newValue.splice(destination.index, 0, targetValue);
+    formAPI.current?.setValue(destination.droppableId, newValue);
+  };
+
   return (
     <main className={styles.page}>
       <Form
@@ -114,67 +138,134 @@ export const PageKatrinaEditor: FC<PageKatrinaEditorProps> = ({
                                 <>
                                   <Collapse
                                     keepDOM
+                                    activeKey={itemsExpanded}
+                                    onChange={(newExpandedItems) => {
+                                      if (
+                                        typeof newExpandedItems === "string"
+                                      ) {
+                                        setItemsExpanded([newExpandedItems]);
+                                      } else {
+                                        setItemsExpanded(
+                                          newExpandedItems ?? []
+                                        );
+                                      }
+                                    }}
                                     expandIconPosition="left"
                                     expandIcon={<IconPlus />}
                                     collapseIcon={<IconMinus />}
                                   >
-                                    <DragDropContext>
-                                      {arrayFieldsItem.map((fieldItem) => (
-                                        <Collapse.Panel
-                                          itemKey={fieldItem.key}
-                                          header={
-                                            <div
-                                              className={styles["item-input"]}
-                                            >
-                                              <Form.Input
-                                                className={classNames(
-                                                  styles["id-input"],
-                                                  styles["item-id"]
-                                                )}
-                                                field={`${fieldItem.field}.id`}
-                                                placeholder={
-                                                  t`faq_id_explanation` as string
-                                                }
-                                                noLabel
-                                                prefix={t`ID`}
-                                                onClick={(e) =>
-                                                  e.stopPropagation()
-                                                }
-                                              />
-                                              <Form.Input
-                                                className={styles["item-title"]}
-                                                field={`${fieldItem.field}.title`}
-                                                noLabel
-                                                placeholder={
-                                                  t`faq_item_title` as string
-                                                }
-                                                suffix={
-                                                  <Button
-                                                    className={
-                                                      styles["item-remove"]
-                                                    }
-                                                    theme="borderless"
-                                                    icon={<IconClose />}
-                                                    type="danger"
-                                                    onClick={fieldItem.remove}
-                                                  />
-                                                }
-                                                onClick={(e) =>
-                                                  e.stopPropagation()
-                                                }
-                                              />
-                                            </div>
-                                          }
-                                        >
-                                          <Form.TextArea
-                                            className={styles["item-content"]}
-                                            field={`${fieldItem.field}.content`}
-                                            noLabel
-                                            autosize
-                                            rows={1}
-                                          />
-                                        </Collapse.Panel>
-                                      ))}
+                                    <DragDropContext
+                                      onDragEnd={handleDragItemEnd}
+                                    >
+                                      <Droppable
+                                        droppableId={`${fieldGroup.field}.contents`}
+                                      >
+                                        {(providedDroppable) => (
+                                          <div
+                                            key={fieldGroup.key}
+                                            ref={providedDroppable.innerRef}
+                                            {...providedDroppable.droppableProps}
+                                          >
+                                            {arrayFieldsItem.map(
+                                              (fieldItem, index) => (
+                                                <Draggable
+                                                  draggableId={fieldItem.field}
+                                                  index={index}
+                                                >
+                                                  {(providedDraggable) => (
+                                                    <div
+                                                      key={fieldItem.key}
+                                                      ref={
+                                                        providedDraggable.innerRef
+                                                      }
+                                                      {...providedDraggable.draggableProps}
+                                                      {...providedDraggable.dragHandleProps}
+                                                    >
+                                                      <Collapse.Panel
+                                                        itemKey={fieldItem.key}
+                                                        header={
+                                                          <div
+                                                            className={
+                                                              styles[
+                                                                "item-input"
+                                                              ]
+                                                            }
+                                                          >
+                                                            <Form.Input
+                                                              className={classNames(
+                                                                styles[
+                                                                  "id-input"
+                                                                ],
+                                                                styles[
+                                                                  "item-id"
+                                                                ]
+                                                              )}
+                                                              field={`${fieldItem.field}.id`}
+                                                              placeholder={
+                                                                t`faq_id_explanation` as string
+                                                              }
+                                                              noLabel
+                                                              prefix={t`ID`}
+                                                              onClick={(e) =>
+                                                                e.stopPropagation()
+                                                              }
+                                                            />
+                                                            <Form.Input
+                                                              className={
+                                                                styles[
+                                                                  "item-title"
+                                                                ]
+                                                              }
+                                                              field={`${fieldItem.field}.title`}
+                                                              noLabel
+                                                              placeholder={
+                                                                t`faq_item_title` as string
+                                                              }
+                                                              suffix={
+                                                                <Button
+                                                                  className={
+                                                                    styles[
+                                                                      "item-remove"
+                                                                    ]
+                                                                  }
+                                                                  theme="borderless"
+                                                                  icon={
+                                                                    <IconClose />
+                                                                  }
+                                                                  type="danger"
+                                                                  onClick={
+                                                                    fieldItem.remove
+                                                                  }
+                                                                />
+                                                              }
+                                                              onClick={(e) =>
+                                                                e.stopPropagation()
+                                                              }
+                                                            />
+                                                          </div>
+                                                        }
+                                                      >
+                                                        <Form.TextArea
+                                                          className={
+                                                            styles[
+                                                              "item-content"
+                                                            ]
+                                                          }
+                                                          field={`${fieldItem.field}.content`}
+                                                          noLabel
+                                                          autosize
+                                                          rows={1}
+                                                        />
+                                                      </Collapse.Panel>
+                                                    </div>
+                                                  )}
+                                                </Draggable>
+                                              )
+                                            )}
+                                            {providedDroppable.placeholder}
+                                          </div>
+                                        )}
+                                      </Droppable>
                                     </DragDropContext>
                                   </Collapse>
                                   <Button
